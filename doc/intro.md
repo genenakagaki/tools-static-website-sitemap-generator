013d5a2c73b781b7e8a923d1a634fd6835b3d098
Gene Nakagaki <genenakagaki@Genes-MacBook-Air.local>
2022-08-21 09:29:14 +0900

    feat: inintialize clojure project
    clojure -T:project/new :template app :name practicalli/my-app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to static-website-sitemap-generator
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tools/static_website_sitemap_generator_test.clj
@@ -0,0 +1,7 @@
+(ns tools.static-website-sitemap-generator-test
+  (:require [clojure.test :refer :all]
+            [tools.static-website-sitemap-generator :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
